af8e959c3bc77f314718187f3bdd8b876d6f7d7d
kimberley <kim.dobney@yahoo.co.uk>
2021-09-28 14:26:34 +0100

    added add button and section div, styled in css and added media query

--- database.php
+++ database.php
@@ -32,11 +32,22 @@ $outputs = $query->fetchAll();
 <head>
   <title>PAPERWEIGHT-WORLD</title>
     <link rel="stylesheet" href="styles.css">
+    <meta name="viewport" content="width=device-width,
+ initial-scale=1.0">
 </head>
-<header>
-    <h1>Welcome to PAPERWEIGHT-WORLD!</h1>
-    <h3>Where no paper is lost</h3>
-    <hr>
-</header>
-
+<body>
+    <header>
+        <h1>Welcome to PAPERWEIGHT-WORLD!</h1>
+        <h3>Where no paper is lost</h3>
+        <hr>
+    </header>
+    <section>
+    <a href="https://www.google.com/search?q=cats&rlz=1C5CHFA_enGB969GB969&oq=cats&aqs=chrome..69i57j46i67i433j0i433i457i512j0i402l2j46i199i291i433i512j46i433i512j0i433i512l2j0i512.977j0j7&sourceid=chrome&ie=UTF-8" target="_blank" >
+        <div class="add">
+            <h1>+</h1>
+            <p>ADD</p>
+        </div>
+    </a>
+    </section>
+</body>
 </html>
