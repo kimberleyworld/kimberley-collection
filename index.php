357f5ad19118fe34da2f6d248eeb41c5ac2640d7
kimberley <kim.dobney@yahoo.co.uk>
2021-09-29 16:54:16 +0100

    created form

--- index.php
+++ index.php
@@ -21,12 +21,12 @@ $display = generateHtml($paperweights);
     <hr>
 </header>
 
-<!--            <div class="weightInfo" id="add">-->
-<!--                <a href="https://www.google.com/search?q=cats&rlz=1C5CHFA_enGB969GB969&oq=cats&aqs=chrome..69i57j46i67i433j0i433i457i512j0i402l2j46i199i291i433i512j46i433i512j0i433i512l2j0i512.977j0j7&sourceid=chrome&ie=UTF-8" target="_blank" >-->
-<!--                <h1>+</h1>-->
-<!--                </a>-->
-<!--                <p>ADD</p>-->
-<!--            </div>-->
+<div class="add">
+    <a href="form.php" target="_blank" >
+        <h1>+</h1>
+    </a>
+    <p>ADD</p>
+</div>
 
 <article>
     <?php
